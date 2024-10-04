c2ce6d3623ed79e03303904be3ffcdc0437ab517
quynezz <laungocquyen098@gmail.com>
2024-10-04 23:19:21 +0700

    DONE

--- 141. Linked List Cycle/LLC.cpp
+++ 141. Linked List Cycle/LLC.cpp
@@ -1,15 +1,15 @@
+#include<bits/stdc++.h> 
 
+using namespace std; 
 
 // Uncomment to run this
 // Using Floyd's Cycle Detection Algorithm
-/**
- * Definition for singly-linked list.
- * struct ListNode {
- *     int val;
- *     ListNode *next;
- *     ListNode(int x) : val(x), next(NULL) {}
- * };
- */
+ struct ListNode {
+     int val;
+     ListNode *next;
+    ListNode(int x) : val(x), next(NULL) {}
+  };
+ 
 class Solution {
 public:
     bool hasCycle(ListNode *head) {
