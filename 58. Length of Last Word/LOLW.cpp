e740bdd5fb4f85ab6b55c8657e2f3333bfb02c2a
quynezz <laungocquyen098@gmail.com>
2024-10-09 23:56:04 +0700

    DONE

---
+++ 58. Length of Last Word/LOLW.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+typedef long long ll; 
+using namespace std;
+
+class Solution {
+public:
+    int lengthOfLastWord(string s) {
+        //  i point at the last element of the string, the first while is loop and stop till it see a ' ', second loop start from the last word till it see the ' '; 
+        int i = s.length(); 
+        int length = 0;
+        while(i > 0 && s[i - 1] == ' '){  
+            i--;
+        }
+        while(i > 0 && s[i - 1] != ' '){  
+            i--; 
+            length++;
+        }
+        return length;
+    }
+};
