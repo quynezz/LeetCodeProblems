99c0dd9835d29d66eda5cff9e76af591e7d24d9c
quynezz <laungocquyen098@gmail.com>
2024-10-14 22:32:44 +0700

    DONE

---
+++ 13. Roman to Integer/bettter.cpp
@@ -0,0 +1,25 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+
+class Solution {
+public:
+    int romanToInt(string s) { 
+unordered_map<char, int> mp{
+            {'I',1},
+            {'V',5},
+            {'X',10},
+            {'L',50},
+            {'C',100},
+            {'D',500},
+            {'M',1000},
+        };   
+        int ans = 0;
+        for(int i = 0 ; i < s.length() ; i++){
+        int sign = mp[s[i]] < mp[s[i + 1]] ? -1 : 1;
+        ans += (sign * mp[s[i]]);
+        }
+       return ans;
+    }
+};
