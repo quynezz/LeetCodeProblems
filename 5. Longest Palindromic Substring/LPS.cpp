85f380820358f15e74a05240d5a27f7e91627e69
quynezz <laungocquyen098@gmail.com>
2024-10-19 23:45:32 +0700

    DONE

---
+++ 5. Longest Palindromic Substring/LPS.cpp
@@ -0,0 +1,31 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+private:
+    string expandCenter(string s, int l, int r) { 
+        while(l >= 0 && r < s.length() && (s[l] == s[r])){  
+            l--; 
+            r++;
+        }
+        return s.substr(l + 1, r - l - 1);
+    }
+public:
+    string longestPalindrome(string s) {
+        string longest = "";
+        if(s.empty()){ 
+            return s;
+        }  
+        for(int i = 0; i < s.length(); i++){  
+            string odd = expandCenter(s,i,i);
+            if(odd.length() > longest.length()){ 
+                longest = odd;
+            }
+            string even = expandCenter(s,i,i + 1);
+            if(even.length() > longest.length()){ 
+                longest = even;
+            }
+        }
+        return longest;
+    }
+};
