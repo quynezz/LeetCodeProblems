f9821d3e63527441b776c00ecc8a36149381b39c
quynezz <laungocquyen098@gmail.com>
2024-10-20 11:16:11 +0700

    DONE

--- 15. 3Sum/3sum.cpp
+++ 15. 3Sum/3sum.cpp
@@ -6,17 +6,16 @@ public:
             return result;
         }
         sort(nums.begin(),nums.end());
-        int n = nums.size();
-        for(int i = 0; i < n; i++){
+        for(int i = 0; i < nums.size(); i++){
             if(i > 0 && nums[i] == nums[i - 1]){
                 continue;
             }
-            for(int j = i + 1; j < n; j++){
+            for(int j = i + 1; j < nums.size(); j++){
                 if(j > i + 1 && nums[j] == nums[j - 1]){
                     continue;
                 }
                 int left = j + 1;
-                int right = n - 1;
+                int right = nums.size() - 1;
                 while(left < right){
                     int sum = nums[i] + nums[j] + nums[left] + nums[right];
                     if(sum == target){
