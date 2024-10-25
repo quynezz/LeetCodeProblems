48997fbf86741922eb4a9155a18f5df2d7857c40
quynezz <laungocquyen098@gmail.com>
2024-10-25 10:23:07 +0700

    DONE

---
+++ 226. Invert Binary Tree/swap.cpp
@@ -0,0 +1,34 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    TreeNode* invertTree(TreeNode* root) {
+        if(root == NULL){
+            return root;
+        } 
+        queue<TreeNode*> q;
+        q.push(root); 
+        while(!q.empty()){ 
+            TreeNode* current = q.front(); 
+            q.pop();
+                swap(current->left,current->right);
+                if(current->left != NULL) {
+                    q.push(current->left);
+                    }
+                if(current->right != NULL){ 
+                q.push(current->right);
+                }
+            }
+        
+        return root;
+    }
+};
