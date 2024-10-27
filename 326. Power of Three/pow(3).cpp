ad69e5a3fde91632689ed20f1b89dec387f7fb88
quynezz <laungocquyen098@gmail.com>
2024-10-28 00:37:26 +0700

    DONE

---
+++ 326. Power of Three/pow(3).cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    bool isPowerOfThree(int n) {
+        if(n == 1){
+            return true;
+        } 
+        if(n == 0 || n < 0){
+            return false;
+        } 
+        while(n % 3 == 0){ 
+            n /= 3;
+            if(n == 1){
+                return true;
+            }
+        }
+        return false;
+    }
+};
