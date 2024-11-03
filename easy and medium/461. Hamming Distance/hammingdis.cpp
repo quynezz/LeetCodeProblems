1b3dde635a8867577b8ae7e71849f57a3fe69ee3
quynezz <laungocquyen098@gmail.com>
2024-11-03 23:05:58 +0700

    DONE

---
+++ easy and medium/461. Hamming Distance/hammingdis.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int hammingDistance(int x, int y) {
+        int count = 0;
+        while( x != 0 || y != 0 ){ 
+            int i = x % 2; 
+            int j = y % 2; 
+            if(i != j){ 
+                count++; 
+            }
+            x >>= 1;
+            y >>= 1;
+        }
+        return count;
+    }
+    // 1 (0 0    0    1)
+    // same dif same dif => 2 dif => ans = 2;
+    // 4 (0 1    0    0)
+
+};
