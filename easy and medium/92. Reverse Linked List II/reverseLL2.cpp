396a82ad71b37305ea21c1173cf1b6c3457d069b
quynezz <laungocquyen098@gmail.com>
2024-11-15 22:42:31 +0700

    DONE

---
+++ easy and medium/92. Reverse Linked List II/reverseLL2.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    ListNode* reverseBetween(ListNode* head, int left, int right) { 
+        ListNode* dummy = new ListNode(0); 
+        dummy->next = head;
+        ListNode* temp = dummy;
+        ListNode* current = head;
+        ListNode* next;
+        ListNode* prev = NULL;
+        for(int i = 1; i < left; i++){ 
+            temp = current;  
+            current = current->next;
+        }
+        for(int i = 0; i < right - left + 1; i++){
+            next = current->next; 
+            current->next = prev;
+            prev = current; 
+            current = next; 
+        }
+        temp->next->next = next;
+        temp->next = prev;
+        return dummy->next;
+    }
+};
