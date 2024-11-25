8fa5d8e452b490ca3d5b19bb98338c8abdf65fe2
quynezz <laungocquyen098@gmail.com>
2024-11-26 00:24:39 +0700

    DONE

--- easy and medium/328. Odd Even Linked List/oddEvenLinkedList.cpp
+++ easy and medium/328. Odd Even Linked List/oddEvenLinkedList.cpp
@@ -58,3 +58,41 @@ public:
         return newHead; 
     }
 };
+
+
+
+
+
+
+
+
+
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+// class Solution {
+// public:
+//     ListNode* oddEvenList(ListNode* head) { 
+//         if(head == NULL || head->next == NULL){
+//             return NULL;
+//         }
+//         ListNode* odd = head;
+//         ListNode* even = head->next;
+//         ListNode* newHead = even;
+//         while(even != NULL && even->next != NULL){
+//             odd->next = odd->next->next;
+//             odd = odd->next;
+//             even->next = even->next->next;
+//             even = even->next;
+//         }
+//         odd->next = newHead;
+//         return head;
+//     }
+// };
