1a1b856856e6e9782c3cd88a474df996cae32a09
quynezz <laungocquyen098@gmail.com>
2024-12-05 11:50:33 +0700

    DONE

--- easy and medium/2337. Move Pieces to Obtain a String/movePieceobtain.cpp
+++ easy and medium/2337. Move Pieces to Obtain a String/movePieceobtain.cpp
@@ -1,32 +1,54 @@
+// class Solution {
+// public:
+//     bool canChange(string start, string target) {   
+//         // first method
+//         vector<pair<char,int>> v1, v2; 
+//         for(int i = 0; i < start.length(); i++){
+//          if(start[i] != '_'){
+//                 v1.push_back({start[i],i});
+//             }
+//         }
+//         for(int i = 0; i < target.length(); i++){
+//             if(target[i] != '_'){
+//                 v2.push_back({target[i],i});
+//             }
+//         }  
+//         if(v1.size() != v2.size()) return false;
+//         for(int i = 0; i < v1.size(); i++){
+//             if(v1[i].first != v2[i].first){
+//                 return false;
+//             }
+//             if(v1[i].first == 'L' && v1[i].second < v2[i].second){
+//                     return false;
+//             }
+//             if(v1[i].first == 'R' && v1[i].second > v2[i].second){ 
+//                  return false;
+//             }
+//         }
+//         return true;
+//     }
+// };
+
 class Solution {
 public:
     bool canChange(string start, string target) {   
-        // first method
-        vector<pair<char,int>> v1, v2; 
-        for(int i = 0; i < start.length(); i++){
-         if(start[i] != '_'){
-                v1.push_back({start[i],i});
-            }
-        }
-        for(int i = 0; i < target.length(); i++){
-            if(target[i] != '_'){
-                v2.push_back({target[i],i});
-            }
-        }  
-        if(v1.size() != v2.size()) return false;
-        for(int i = 0; i < v1.size(); i++){
-            if(v1[i].first != v2[i].first){
-                return false;
-            }
-            if(v1[i].first == 'L' && v1[i].second < v2[i].second){
-                    return false;
-            }
-            if(v1[i].first == 'R' && v1[i].second > v2[i].second){ 
-                 return false;
-            }
+      int i = 0, j = 0;
+        int n = start.length();
+        int m = target.length();
+        while(i < n || j < m){
+            while(i < n && start[i] == '_') i++;
+            while(j < m && target[j] == '_') j++;
+            if(i == n && j == n) return true;
+            if(i == n || j == n || start[i] != target[j]) return false;
+            if(start[i] == 'L' && i < j || start[i] == 'R' && i > j) return false;
+            i++;
+            j++;
         }
         return true;
     }
 };
+
+
+
 
 
--- easy and medium/777. Swap Adjacent in LR String/swapAdj.cpp
+++ easy and medium/777. Swap Adjacent in LR String/swapAdj.cpp
@@ -1,28 +1,51 @@
+// class Solution {
+// public:
+//     bool canTransform(string start, string result) {
+//             vector<pair<char,int>> v1, v2; 
+//         for(int i = 0; i < start.length(); i++){
+//          if(start[i] != 'X'){
+//                 v1.push_back({start[i],i});
+//             }
+//         }
+//         for(int i = 0; i < result.length(); i++){
+//             if(result[i] != 'X'){
+//                 v2.push_back({result[i],i});
+//             }
+//         }  
+//         if(v1.size() != v2.size()) return false;
+//         for(int i = 0; i < v1.size(); i++){
+//             if(v1[i].first != v2[i].first){
+//                 return false;
+//             }
+//             if(v1[i].first == 'L' && v1[i].second < v2[i].second){
+//                     return false;
+//             }
+//             if(v1[i].first == 'R' && v1[i].second > v2[i].second){ 
+//                  return false;
+//             }
+//         }
+//         return true;
+//     }
+// };
+
+
+
+
+
 class Solution {
 public:
-    bool canTransform(string start, string result) {
-            vector<pair<char,int>> v1, v2; 
-        for(int i = 0; i < start.length(); i++){
-         if(start[i] != 'X'){
-                v1.push_back({start[i],i});
-            }
-        }
-        for(int i = 0; i < result.length(); i++){
-            if(result[i] != 'X'){
-                v2.push_back({result[i],i});
-            }
-        }  
-        if(v1.size() != v2.size()) return false;
-        for(int i = 0; i < v1.size(); i++){
-            if(v1[i].first != v2[i].first){
-                return false;
-            }
-            if(v1[i].first == 'L' && v1[i].second < v2[i].second){
-                    return false;
-            }
-            if(v1[i].first == 'R' && v1[i].second > v2[i].second){ 
-                 return false;
-            }
+    bool canTransform(string start, string result) { 
+        int i = 0, j = 0;
+        int n = start.length();
+        int m = result.length();
+        while(i < n || j < m){
+            while(i < n && start[i] == 'X') i++;
+            while(j < m && result[j] == 'X') j++;
+            if(i == n && j == n) return true;
+            if(i == n || j == n || start[i] != result[j]) return false;
+            if(start[i] == 'L' && i < j || start[i] == 'R' && i > j) return false;
+            i++;
+            j++;
         }
         return true;
     }
