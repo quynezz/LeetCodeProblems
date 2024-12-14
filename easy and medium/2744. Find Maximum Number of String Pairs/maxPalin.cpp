839a7f094ffcb6dbfec53136cd79fdaaa910ec9b
quynezz <laungocquyen098@gmail.com>
2024-12-15 00:02:00 +0700

    DONE

---
+++ easy and medium/2744. Find Maximum Number of String Pairs/maxPalin.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int maximumNumberOfStringPairs(vector<string>& words) {
+        int count = 0;
+        for(int i = 0; i < words.size() - 1; i++){
+            for(int j = i + 1; j < words.size(); j++){
+                string temp1 = words[i];
+                string temp2 = words[j];
+                reverse(temp1.begin(),temp1.end());
+                reverse(temp2.begin(),temp2.end());
+                if(words[i] == words[j] || words[i] == temp2 || words[j] == temp1){
+                    count++;
+                } 
+            }
+        }
+        return count;
+    }
+};
