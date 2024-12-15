853aa2ccad59bfa3eee2b612f9535c579cc326ae
quynezz <laungocquyen098@gmail.com>
2024-12-15 23:56:04 +0700

    DONE

---
+++ easy and medium/1460. Make Two Arrays Equal by Reversing Subarrays/ez.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    bool canBeEqual(vector<int>& target, vector<int>& arr) {
+        unordered_map<int,int> m1;
+        unordered_map<int,int> m2; 
+        for(auto it : target){
+            m1[it]++;
+        }
+        for(auto it : arr){
+            m2[it]++;
+        }
+        for(auto it : arr){
+            if(m1[it] != m2[it] || m1.find(it) == m1.end()){
+                return false;
+            }
+        }
+        return true;
+    }
+};
