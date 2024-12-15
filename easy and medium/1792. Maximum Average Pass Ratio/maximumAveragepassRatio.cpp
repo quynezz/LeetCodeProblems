ebfde795d7f393a7b7ddd78ae59cc1fa0a81f8ba
quynezz <laungocquyen098@gmail.com>
2024-12-15 22:54:54 +0700

    DONE

--- easy and medium/1792. Maximum Average Pass Ratio/maximumAveragepassRatio.cpp
+++ easy and medium/1792. Maximum Average Pass Ratio/maximumAveragepassRatio.cpp
@@ -1,6 +1,9 @@
 class Solution {
 public:
     double maxAverageRatio(vector<vector<int>>& classes, int extraStudents) { 
+        // đầu tiên thêm vào từng lớp 1 student chắc chắn pass
+        // tính profit ratio bằng cách  tính ratio lúc cộng 1  -  ratio ban đầu => ra được profit
+        // thêm profit vào PQ rồi tiếp tục update PQ dựa trên ratio sau khi cộng 1; 
         double sum = 0;
         auto profit = [&](int pass, int total) { 
             return ((pass + 1) * 1.0 / (total + 1) * 1.0) - (pass * 1.0 / total);
