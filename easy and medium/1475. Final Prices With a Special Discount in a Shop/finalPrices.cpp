1bc17d731ec7c8f575750bc0bccb0c103901f04d
quynezz <laungocquyen098@gmail.com>
2024-12-18 11:39:34 +0700

    DONE

--- easy and medium/1475. Final Prices With a Special Discount in a Shop/finalPrices.cpp
+++ easy and medium/1475. Final Prices With a Special Discount in a Shop/finalPrices.cpp
@@ -13,3 +13,33 @@ public:
         return v;
     }
 };
+
+
+
+// brute force 
+
+
+class Solution {
+public:
+    vector<int> finalPrices(vector<int>& prices) {
+        vector<int> v;
+        for(int i = 0; i < prices.size() - 1; i++){
+            int temp = 0;
+            bool discount = false;
+            for(int j = i + 1; j < prices.size(); j++){
+                if(j > i && prices[j] <= prices[i]){
+                    temp = prices[i] - prices[j];
+                    discount = true;
+                    break;
+                }
+            }
+                if(discount == true){
+                    v.push_back(temp);
+                }else{
+                    v.push_back(prices[i]);
+                }
+        }
+        v.push_back(prices[prices.size() - 1]);
+        return v;
+    }
+};
