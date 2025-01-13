9716e995ef64edeea51e513f55e64582ab99b353
quynezz <laungocquyen098@gmail.com>
2025-01-13 22:57:27 +0700

    DONE

---
+++ easy and medium/3216. Lexicographically Smallest String After a Swap/lexSwap.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    string getSmallestString(string s) {
+        for(int i = 0; i < s.size() - 1; i++){
+            if(((s[i] - '0') % 2 == (s[i + 1] - '0') % 2) && s[i] > s[i + 1]){
+                swap(s[i],s[i + 1]);
+                break;
+            }
+        }
+        return s;
+    }
+};
