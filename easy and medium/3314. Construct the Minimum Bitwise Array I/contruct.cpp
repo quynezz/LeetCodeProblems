31c1bd567e45fc1f23078b1d9e363c28f6c74543
quynezz <laungocquyen098@gmail.com>
2025-02-01 17:42:14 +0700

    DONE

---
+++ easy and medium/3314. Construct the Minimum Bitwise Array I/contruct.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    vector<int> minBitwiseArray(vector<int>& nums) {
+        vector<int> v;
+        for(int i = 0; i < nums.size(); i++){
+            int cur = nums[i];
+            int ans = -1;
+            for(int j = 0; j < nums[i]; j++){
+                int temp = j | (j + 1);
+                if(temp == cur){
+                    ans = j;
+                    break;
+                }
+            }
+            v.push_back(ans);
+        }
+        return v;
+    }
+};
