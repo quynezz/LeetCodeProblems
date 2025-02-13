81d7e3c6345fed9c395b3152123bfbb3454ca46e
quynezz <laungocquyen098@gmail.com>
2025-02-13 12:16:11 +0700

    DONE

--- easy and medium/3066. Minimum Operations to Exceed Threshold Value II/minimum.cpp
+++ easy and medium/3066. Minimum Operations to Exceed Threshold Value II/minimum.cpp
@@ -1,17 +1,38 @@
+#include <bits/stdc++.h>
+#include <unordered_set>
+using namespace std;
+typedef long long ll;
+const int maxN = 1000001;
+const int MOD = 1e9;
+#define vii vector<pair<int, int>>
+
 class Solution {
-public:
-    int minOperations(vector<int>& nums, int k) {
-        priority_queue<long long,vector<long long>,greater<>> pq(nums.begin(),nums.end());
-        long long count = 0;
-        while(pq.top() < k){
-            long long n = pq.top();
-            pq.pop();
-            long long m = pq.top();
-            pq.pop();
-            long long temp = min(n,m) * 2 + max(n,m);
-            count++;
-            pq.push(temp);
+    public:
+        int minOperations(vector<int>& nums, int k) {
+            int count = 0;
+            priority_queue<long long,vector<long long>,greater<>> pq(nums.begin(),nums.end());
+            while(pq.top() < k){
+                long long n = pq.top(); 
+                pq.pop(); 
+                long long m = pq.top();
+                pq.pop();
+                pq.push(min(n,m) * 2 + max(n,m));
+                count++;
+            }
+            // cout << count << endl;
+            return count;
         }
-        return count;
-    }
-};
+    };
+
+auto speedup = []() {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+    return 0;
+}();
+// int main() {   
+//     Solution s; 
+//     vector<int> nums = {1,1,2,4,9};
+//     int k = 20;
+//     s.minOperations(nums,k);
+// }
