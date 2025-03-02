96fc8a1e865f578078c203a00560678dd9be5a8c
quynezz <laungocquyen098@gmail.com>
2025-03-02 10:38:23 +0700

    FINISH

--- easy and medium/2570. Merge Two 2D Arrays by Summing Values/merge2darray.cpp
+++ easy and medium/2570. Merge Two 2D Arrays by Summing Values/merge2darray.cpp
@@ -1,24 +1,29 @@
 class Solution {
-public:
-    vector<vector<int>> mergeArrays(vector<vector<int>>& nums1, vector<vector<int>>& nums2) {
-        vector<vector<int>> v1;
-        for (auto& it : nums2) {
-            nums1.push_back({it[0], it[1]});
-        }
-        sort(nums1.begin(), nums1.end(), [](vector<int>& a, vector<int>& b) {
-            return a[0] < b[0];
-        });
-        int i = 0;
-        while (i < nums1.size()) {
-            if (i + 1 < nums1.size() && nums1[i][0] == nums1[i + 1][0]) {
-                nums1[i][1] += nums1[i + 1][1];
-                nums1.erase(nums1.begin() + i + 1);
-            } else {
-                v1.push_back({nums1[i][0], nums1[i][1]});
-                i++; 
+    public:
+        vector<vector<int>> mergeArrays(vector<vector<int>>& nums1, vector<vector<int>>& nums2) {
+            unordered_map<int,int> m;
+            vector<vector<int>> v;
+            for(auto it : nums1){
+                int idx = it[0];
+                int val = it[1];
+                m[idx] = val;
             }
+            for(auto it : nums2){
+                int idx = it[0];
+                int val = it[1];
+                if(m.count(idx)){
+                    m[idx] += val;
+                }else{
+                    m[idx] = val;
+                }
+            }
+            for(auto& [idx , val] : m){
+                v.push_back({idx,val});
+            }
+    
+            sort(v.begin(),v.end(),[&](vector<int> a,vector<int>b){
+                return a[0] < b[0];
+            });
+            return v;
         }
-
-        return v1;
-    }
-};
+    };
