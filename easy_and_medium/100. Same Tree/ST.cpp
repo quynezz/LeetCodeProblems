2d4f739994b572e18ece45df4499052073a3daa8
quynezz <laungocquyen098>
2025-11-23 22:12:08 +0700

    back to my primeeee

--- easy_and_medium/100. Same Tree/ST.cpp
+++ easy_and_medium/100. Same Tree/ST.cpp
@@ -1,10 +1,10 @@
 class Solution {
 public:
     bool isSameTree(TreeNode* p, TreeNode* q) {
-        // Base cases
+        // Base cases --> make sure it alright
         if (!p && !q) return true;  // Both are null
         if (!p || !q) return false; // One is null, but the other is not
-        return (p->val == q->val) && 
+        return (p->val == q->val) &&
                isSameTree(p->left, q->left) &&
                isSameTree(p->right, q->right);
     }
